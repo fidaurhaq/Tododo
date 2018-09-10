ad0eb21bc59e1c0b70c8a92269f75175c9418c8a
fidaurhaq <fidaur.haq@entelect.co.za>
2018-09-10 11:58:12 +0200

    Added a css loader and render based on state of loading value

--- src/components/todoList/TodoList.vue
+++ src/components/todoList/TodoList.vue
@@ -1,18 +1,19 @@
 <template>
   <div>
-    <div class="columns box">
-      <div class="column">
-        <h3><strong>Doing</strong></h3>
-        <div v-for="todoItem in allTodoes" v-if="todoItem.doneStatus!==true" v-bind:key="todoItem.id"> 
-          <TodoListItem v-bind:item="todoItem"/>
+    <div class="lds-hourglass" v-if="loading===1"/>
+    <div class="columns box" v-if="loading===0">
+        <div class="column">
+          <h3><strong>Doing</strong></h3>
+          <div v-for="todoItem in allTodoes" v-if="todoItem.doneStatus!==true" v-bind:key="todoItem.id"> 
+            <TodoListItem v-bind:item="todoItem"/>
+          </div>
         </div>
-      </div>
-      <div class="column">
-        <h3><strong>Done</strong></h3>
-        <div v-for="todoItem in allTodoes" v-if="todoItem.doneStatus===true" v-bind:key="todoItem.id"> 
-          <TodoListItem v-bind:item="todoItem"/>
+        <div class="column">
+          <h3><strong>Done</strong></h3>
+          <div v-for="todoItem in allTodoes" v-if="todoItem.doneStatus===true" v-bind:key="todoItem.id"> 
+            <TodoListItem v-bind:item="todoItem"/>
+          </div>
         </div>
-      </div>
     </div>
   </div>
 </template>
